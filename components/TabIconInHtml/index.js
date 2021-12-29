cb0415e53772f2a2fd09c278a04e6e60d354d013
Nelson Nery <nelsonmanu2@gmail.com>
2021-12-29 16:35:52 -0400

    temp

--- components/TabIconInHtml/index.js
+++ components/TabIconInHtml/index.js
@@ -6,10 +6,10 @@ const TabIconInHtml = ({src=''}) => {
 	return (
 		<Box id="tab-icon-html">
 			<Head>
-				<link rel="icon" href="/resources/beer.png" type="image/x-icon" />
+				<link rel="icon" href="/resources/beer.png" type="image/png" sizes="64x64" />
 			</Head>
 			<Heading>TAB ICON IN HTML</Heading>
-			<Code colorScheme="facebook" children={'<head><link rel="icon" href="..." type="image/x-icon" /></head>'} />
+			<Code colorScheme="facebook" children={'<head><link rel="icon" href="image.png" type="image/png" sizes="64x64" /></head>'} />
 			<Image htmlWidth={200} src="/resources/tab-icon-html.png"/>
 		</Box>	
 	)
